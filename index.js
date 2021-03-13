2b7b3074e9bd091fe22c703324c585af3fc4d319
chugjug16 <79376277+chugjug16@users.noreply.github.com>
2021-03-12 23:24:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ io.on('connection', (socket) => {
         // //[//console.log(socket)
  //        // //[//console.log(socket.nickname)
         socket.ishost = socket.id;
-        random_num = Math.floor(Math.random() * 860) + 1  
+        random_num = Math.floor(Math.random() * 965) + 1  
          for (socketID in io.nsps['/'].adapter.rooms[room_obj[0]].sockets) {
             const nickname = io.nsps['/'].connected[socketID].nickname;
             // io.nsps['/'].connected[socketID].nickname
